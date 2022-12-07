0f74013a122188f5d13cadbda0030a47c6da820a
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-07 15:43:44 +0100

    Added displaying error handling. Closes #50

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,15 +4,19 @@ import axios from "axios";
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', {
-            email, password
-        })
-
-        console.log(response.data);
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            });
+            console.log(response.data);
+        } catch (err) {
+            setErrors(err.response.data.errors);
+        }
     };
 
     return <form onSubmit={onSubmit}>
@@ -25,6 +29,14 @@ const Signup = () => {
             <label>Password</label>
             <input value={password} onChange={e => setPassword(e.target.value)} type={"password"} className="form-control" />
         </div>
+        {errors.length > 0 &&(
+            <div className="alert alert-danger">
+                <h4>Oooops...</h4>
+                <ul className="my-0">
+                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+            </div>
+            )}
         <button className="btn btn-primary">Sign Up</button>
     </form>
 };
