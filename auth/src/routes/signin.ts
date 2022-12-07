aafa9bd9b364b4ec72be632dbf291d2a535e20a8
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-07 11:32:34 +0100

    Added Signinlogic Closes #31

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, {Request, Response} from "express";
 import {body} from "express-validator";
+import {Password} from "../services/password";
+import {User} from "../models/user";
 import {validateRequest} from "../middlewares/validate-request";
+import {BadRequestError} from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -14,8 +18,33 @@ router.post('/api/users/signin', [
         .withMessage('You must supply a password')
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid Username or Password');
+        }
+
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid Username or Password');
+        }
+
+        const userJWT = jwt.sign({
+                id: existingUser.id,
+                email: existingUser.email
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store it on the Session
+        req.session = {
+            jwt: userJWT
+        };
+
+        res.send(200).send(existingUser);
 });
 
 export { router as signinRouter };
