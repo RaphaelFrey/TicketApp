d7df84a7e14c8bfa8b29cb48d8455e45977e7524
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-07 17:20:06 +0100

    Implemented onSuccess Callback. Closes #52

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -8,13 +8,17 @@ import {useState} from "react";
  * @param body
  * @returns {{doRequest: ((function(): Promise<void>)|*), errors: unknown}}
  */
-const useRequest = ({url, method, body}) => {
+const useRequest = ({url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
 
     const doRequest = async () => {
         try {
             setErrors(null);
             const response = await axios[method](url, body);
+
+            if (onSuccess) {
+                onSuccess(response.data);
+            }
             return response.data;
         } catch (err) {
             setErrors(
