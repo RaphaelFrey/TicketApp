72f6b3b561d5757019ce4ce292be1f1a11ca06d5
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-07 18:52:46 +0100

    Passing through the whole header. Also replaced the hardcoded host part. Closes #54

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,7 +8,7 @@ const LandingPage = ({currentUser}) => {
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
 
     if (typeof window === 'undefined') {
         //we are on the server!
@@ -16,9 +16,7 @@ LandingPage.getInitialProps = async () => {
         //request should be made to ingress-nginx
         const { data } = await axios.get(
             'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
+                headers: req.headers
             }
         );
     }
