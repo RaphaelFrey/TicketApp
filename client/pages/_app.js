51c58f221bb6df962eab4f1c1a5140056ba1e799
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-07 20:31:37 +0100

    Implemented Sign out. Closes #58

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const AppComponent =  ({Component, pageProps}) => {
+const AppComponent =  ({Component, pageProps, currentUser}) => {
     return (<div>
-        <h1>Header!</h1>
+        <Header currentUser={currentUser} />
         <Component {...pageProps} />
     </div>);
 };
@@ -17,7 +18,10 @@ AppComponent.getInitialProps = async (appContext) => {
         pageProps = await appContext.Component.getInitialProps(appContext.ctx);
     }
 
-    return data;
+    return {
+        pageProps,
+        ...data
+    };
 };
 
 export default AppComponent;
