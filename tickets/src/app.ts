91eb48252c05dc9833684ec42e9f371ce9de4959
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-12-08 20:42:01 +0100

    Added some lines to make Error logging and handling better. Closes #65 Also Implemented index.ts with passing tests in index.test.ts. Closes #67

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import {currentUser, errorHandler, NotFoundError} from '@raphaelfreysolutions/commons';
 import {createTicketRouter} from "./routes/new";
 import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
 
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
